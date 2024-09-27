3f8666221402f53831deab6e2fd8064fbc9897ec
diya krishna <diyakrishna692@gmail.com>
2024-09-27 10:05:13 +0530

    commit

--- src/Project/NavBar.jsx
+++ src/Project/NavBar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">NavBar</Navbar.Brand>
+        <Navbar.Brand href="#home">E-Commerce</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav style={{ marginLeft: "auto" }}>
